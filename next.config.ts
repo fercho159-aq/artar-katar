2a472bee06a02b61e3f3da1c796f8b14a54bd5d0
Fernando Trejo <fernandotrejo159@gmail.com>
2025-12-03 14:48:56 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'pulserasportalviviente.vercel.app',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
